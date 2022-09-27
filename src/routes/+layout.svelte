aca756480ca2ebb51328a6ee7bd438c3e763df2f
Ronald Nababan <ronsen@startrek.or.id>
2022-09-27 18:40:30 +0700

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
     import "../app.css";
 </script>
 
-<div class="container mx-auto my-10 px-10">
+<nav class="bg-white shadow px-3 py-2 font-bold">
+    <div class="container mx-auto text-center">
+        <a href="/" class="title text-2xl">Bibel</a>
+    </div>
+</nav>
+
+<main class="container mx-auto mt-3 mb-12 px-3">
     <slot />
-</div>
+</main>
+
+<footer class="fixed left-0 bottom-0 w-full text-center text-xs p-2 bg-zinc-900 text-white opacity-50">
+    <a href="https://s.id/ronsen" target="_blank">Built with &hearts; by Ronald Nababan</a>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,28 @@
     <title>Bibel - Alkitab bahasa Batak Toba</title>
 </svelte:head>
 
-{#each Books as book}
-    <div class="flex justify-between mb-3">
-        <a href="/buku/{book.slug}">{book.name}</a>
-        {book.number_of_chapters}
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div>
+        <h3 class="font-bold border-b pb-2 mb-2">Padan Na Robi</h3>
+        {#each Books as book}
+            {#if book.category_id == 1}
+            <div class="flex justify-between border-b pb-2 mb-2">
+                <a href="/buku/{book.slug}">{book.name}</a>
+                {book.number_of_chapters}
+            </div>
+            {/if}
+        {/each}
     </div>
-{/each}
+
+    <div>
+        <h3 class="font-bold border-b pb-2 mb-2">Padan Na Imbaru</h3>
+        {#each Books as book}
+            {#if book.category_id == 2}
+            <div class="flex justify-between border-b pb-2 mb-2">
+                <a href="/buku/{book.slug}">{book.name}</a>
+                {book.number_of_chapters}
+            </div>
+            {/if}
+        {/each}
+    </div>
+</div>
