c7b4c37d4f85696ee7b592ea31c2f1c01abfc5be
Ronald Nababan <ronsen@startrek.or.id>
2022-09-27 18:43:26 +0700

    update

--- src/routes/+page.server.js
+++
@@ -1,9 +0,0 @@
-import Books from "$lib/books.json";
-
-/** @type {import('./$types').PageLoad} */
-export function load({ params }) {
-    let oldBooks = Books.filter((book) => book.category_id == 1);
-    let newBooks = Books.filter((book) => book.category_id == 2);
-
-    return { oldBooks, newBooks };
-}
