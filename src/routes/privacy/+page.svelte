d57185e5ee5e1967ddff60de380777cf73b470d0
Ronald Nababan <ronsen@startrek.or.id>
2022-09-29 10:12:59 +0700

    update

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,68 @@
+<h3 class="font-bold text-2xl border-b pb-2 mb-2">Privacy Policy</h3>
+
+<p class="mb-3">
+    Bibel operates the https://bibel.vercel.app/ website, which provides the
+    SERVICE.
+</p>
+
+<p class="mb-3">
+    This page is used to inform website visitors regarding our policies with the
+    collection, use, and disclosure of Personal Information if anyone decided to
+    use our Service, the Bibel website.
+</p>
+
+<p class="mb-3">
+    If you choose to use our Service, then you agree to the collection and use
+    of information in relation with this policy. The Personal Information that
+    we collect are used for providing and improving the Service. We will not use
+    or share your information with anyone except as described in this Privacy
+    Policy.
+</p>
+
+<p class="mb-3">
+    The terms used in this Privacy Policy have the same meanings as in our Terms
+    and Conditions, which is accessible at https://bibel.vercel.app/, unless
+    otherwise defined in this Privacy Policy.
+</p>
+
+<h4 class="text-lg mb-3">Information Collection and Use</h4>
+
+<p class="mb-3">
+    For a better experience while using our Service, we may require you to
+    provide us with certain personally identifiable information, including but
+    not limited to your name, phone number, and postal address. The information
+    that we collect will be used to contact or identify you.
+</p>
+
+<h4 class="text-lg mb-3">Log Data</h4>
+
+<p class="mb-3">
+    We want to inform you that whenever you visit our Service, we collect
+    information that your browser sends to us that is called Log Data. This Log
+    Data may include information such as your computer’s Internet Protocol
+    ("IP") address, browser version, pages of our Service that you visit, the
+    time and date of your visit, the time spent on those pages, and other
+    statistics.
+</p>
+
+<h4 class="text-lg mb-3">Cookies</h4>
+
+<p class="mb-3">
+    Cookies are files with small amount of data that is commonly used an
+    anonymous unique identifier. These are sent to your browser from the website
+    that you visit and are stored on your computer’s hard drive.
+</p>
+
+<p class="mb-3">
+    Our website uses these "cookies" to collection information and to improve
+    our Service. You have the option to either accept or refuse these cookies,
+    and know when a cookie is being sent to your computer. If you choose to
+    refuse our cookies, you may not be able to use some portions of our Service.
+</p>
+
+<h4 class="text-lg mb-3">Contact Us</h4>
+
+<p class="mb-3">
+    If you have any questions or suggestions about our Privacy Policy, do not
+    hesitate to contact us.
+</p>
