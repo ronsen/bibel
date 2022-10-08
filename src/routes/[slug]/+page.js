24f4a55ff5d1cd4c58e8b10da5e46a96cd215d55
Ronsen <ronsen@startrek.or.id>
2022-10-08 23:07:59 +0700

    update

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -2,7 +2,7 @@ import Books from "$lib/books.json";
 import Chapters from "$lib/chapters.json";
 
 /** @type {import('./$types').PageLoad} */
-export function load({ params }) {
+export const load = ({ params }) => {
     let book = Books.find((b) => b.slug == params.slug);
     let chapters = Chapters.filter((chapter) => chapter.book_id == book.id);
 
--- src/routes/[slug]/[chapter]/+page.js
+++ src/routes/[slug]/[chapter]/+page.js
@@ -3,7 +3,7 @@ import Chapters from "$lib/chapters.json";
 import Verses from "$lib/verses.json";
 
 /** @type {import('./$types').PageLoad} */
-export function load({ params }) {
+export const load = ({ params }) => {
     let book = Books.find((b) => b.slug == params.slug);
     let chapter = Chapters.find((c) => c.book_id == book.id && c.number == params.chapter);
     let verses = Verses.filter((verse) => verse.chapter_id == chapter.id);
