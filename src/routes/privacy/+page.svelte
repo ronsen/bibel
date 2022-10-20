df7a4e5d0350ec394bbdd40cf92ab38d4a3d5d98
Ronsen <ronsen@startrek.or.id>
2022-10-20 16:27:52 +0700

    update

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -25,41 +25,6 @@
     otherwise defined in this Privacy Policy.
 </p>
 
-<h4 class="text-lg mb-3">Information Collection and Use</h4>
-
-<p class="mb-3">
-    For a better experience while using our Service, we may require you to
-    provide us with certain personally identifiable information, including but
-    not limited to your name, phone number, and postal address. The information
-    that we collect will be used to contact or identify you.
-</p>
-
-<h4 class="text-lg mb-3">Log Data</h4>
-
-<p class="mb-3">
-    We want to inform you that whenever you visit our Service, we collect
-    information that your browser sends to us that is called Log Data. This Log
-    Data may include information such as your computer’s Internet Protocol
-    ("IP") address, browser version, pages of our Service that you visit, the
-    time and date of your visit, the time spent on those pages, and other
-    statistics.
-</p>
-
-<h4 class="text-lg mb-3">Cookies</h4>
-
-<p class="mb-3">
-    Cookies are files with small amount of data that is commonly used an
-    anonymous unique identifier. These are sent to your browser from the website
-    that you visit and are stored on your computer’s hard drive.
-</p>
-
-<p class="mb-3">
-    Our website uses these "cookies" to collection information and to improve
-    our Service. You have the option to either accept or refuse these cookies,
-    and know when a cookie is being sent to your computer. If you choose to
-    refuse our cookies, you may not be able to use some portions of our Service.
-</p>
-
 <h4 class="text-lg mb-3">Contact Us</h4>
 
 <p class="mb-3">
